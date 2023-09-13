84127fc2ab6e9e147d6e01e3136fc8a6714f9a72
Chris Berry <chrisdberry@protonmail.com>
2023-09-13 17:33:24 +0200

    emitter/emitter.py Fixes

--- emitter/emitter.py
+++ emitter/emitter.py
@@ -7,7 +7,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-d', '--delay', type=int, default=1000, help='Delay in ms between requests')
 parser.add_argument('-f', '--file', type=str, default='DBdataset.csv', help='File to read data from')
-parser.add_argument('-u', '--url', type=str, default="http://web:8000/api/sensor/upload-sensor-data/", help='File to read data from')
+parser.add_argument('-u', '--url', type=str, default="http://web:8000/api/sensor/upload-sensor-data/", help='URL endpoint to send data to')
 args = parser.parse_args()
 
 url = args.url
