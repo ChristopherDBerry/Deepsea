f804f1a808dc672cbe9104f3b3d090a44c1000e5
Chris Berry <chrisdberry@protonmail.com>
2023-09-15 12:29:21 +0200

    Docker tidy up

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# python manage.py flush --no-input
+python manage.py flush --noinput
 python manage.py migrate
+
+# Create the superuser
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('root', 'admin@example.com', 'root')" | python manage.py shell
+
 
 exec "$@"
